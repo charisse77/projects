172258f59ec5848dc2bbc3e3d4f955d7b635b5f9
charisse77 <89757730+charisse77@users.noreply.github.com>
2022-05-17 21:40:06 -0500

    dbconnect works

--- s-and-l/dbConnect.php
+++ s-and-l/dbConnect.php
@@ -1,20 +1,17 @@
 <?php
 //This file creates a 'connection object' to our database
 
-$database = "wdv341"; //name of database
+$database = "s_and_l"; //name of database
 $serverName = "localhost"; //most default to localhost
 $username = "root"; //username for the database NOT your account
 $password = ""; //pw for the database NOT your account
 
 try {
 	$conn = new PDO("mysql:host=$serverName;dbname=$database", $username, $password);
-	$conn->setAttribute(PDo::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-	
-	
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
-	error_log($e->getMessage());
+    error_log($e->getMessage());
 	header("location:error.php");
 	exit;
 };
